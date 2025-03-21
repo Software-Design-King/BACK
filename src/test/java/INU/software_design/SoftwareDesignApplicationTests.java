57ff8fc79015ada99e705e540953ddb5efc71442
JayongLee <dlwpdyd201@naver.com>
2025-03-21 14:26:33 +0900

    프로젝트 생성

---
+++ src/test/java/INU/software_design/SoftwareDesignApplicationTests.java
@@ -0,0 +1,13 @@
+package INU.software_design;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoftwareDesignApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
