6b9e8c80107777dee9f3ba6c3d3d9ddc563fbe8a
Jeyong <dlwpdyd201@naver.com>
2025-04-09 12:01:07 +0900

    [fix] 열거형에 한국어 값 및 JSON 변환 메서드 추가

--- src/main/java/INU/software_design/common/enums/AttendanceType.java
+++ src/main/java/INU/software_design/common/enums/AttendanceType.java
@@ -1,7 +1,31 @@
 package INU.software_design.common.enums;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+
 public enum AttendanceType {
-    LATE,
-    LEAVE,
-    ABSENT
+    LATE("지각"),
+    LEAVE("조퇴"),
+    ABSENT("결석");
+
+    private final String value;
+
+    AttendanceType(String value) {
+        this.value = value;
+    }
+
+    @JsonValue
+    public String getValue() {
+        return value;
+    }
+
+    @JsonCreator
+    public static AttendanceType fromValue(String value) {
+        for (AttendanceType type : AttendanceType.values()) {
+            if (type.value.equals(value)) {
+                return type;
+            }
+        }
+        throw new IllegalArgumentException("정의되지 않은 값 입니다 : " + value);
+    }
 }
