38168d650f886c169ec10e25db6d526373e80d7f
Jeyong <dlwpdyd201@naver.com>
2025-04-09 12:03:59 +0900

    [feat] 학생 기본 정보, 출결, 리스트업 로직 구현 및 컨트롤러 작성

--- src/main/java/INU/software_design/domain/teacher/TeacherController.java
+++ src/main/java/INU/software_design/domain/teacher/TeacherController.java
@@ -1,10 +1,14 @@
 package INU.software_design.domain.teacher;
 
+import INU.software_design.common.resolver.UserId;
 import INU.software_design.common.response.ApiResponseUtil;
 import INU.software_design.common.response.BaseResponse;
 import INU.software_design.common.response.code.SuccessCode;
 import INU.software_design.domain.score.ScoreService;
 import INU.software_design.domain.score.dto.request.StudentScoreRequest;
+import INU.software_design.domain.student.StudentService;
+import INU.software_design.domain.student.dto.request.AttendanceRequest;
+import INU.software_design.domain.student.dto.request.StudentInfoRequest;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -16,6 +20,14 @@ import org.springframework.web.bind.annotation.*;
 public class TeacherController {
 
     private final ScoreService scoreService;
+    private final StudentService studentService;
+
+    @GetMapping("/student/list")
+    public ResponseEntity<BaseResponse<?>> getStudentList(
+            @UserId Long teacherId
+            ) {
+        return ApiResponseUtil.success(SuccessCode.OK, studentService.getStudentList(teacherId));
+    }
 
     @PostMapping("/student/score/{studentId}")
     public ResponseEntity<BaseResponse<?>> registerScore(
@@ -40,5 +52,36 @@ public class TeacherController {
             ) {
         scoreService.deleteStudentScore(studentId, semester);
         return ApiResponseUtil.success(SuccessCode.OK);
+    }
+
+    @GetMapping("/student/info/{studentId}")
+    public ResponseEntity<BaseResponse<?>> getStudentInfo(
+            @PathVariable(value = "studentId") Long studentId
+            ) {
+        return ApiResponseUtil.success(SuccessCode.OK, studentService.getStudentInfo(studentId));
+    }
+
+    @PatchMapping("/student/info/{studentId}")
+    public ResponseEntity<BaseResponse<?>> updateStudentInfo(
+            @PathVariable(value = "studentId") Long studentId,
+            @Valid @RequestBody StudentInfoRequest request
+            ) {
+        return ApiResponseUtil.success(SuccessCode.OK, studentService.updateStudentInfo(studentId, request));
+    }
+
+    @PostMapping("/student/attendance/{studentId}")
+    public ResponseEntity<BaseResponse<?>> registerAttendance(
+            @PathVariable(value = "studentId") Long studentId,
+            @Valid @RequestBody AttendanceRequest request
+            ) {
+        return ApiResponseUtil.success(SuccessCode.OK, studentService.registerAttendance(studentId, request));
+    }
+
+    @PatchMapping("/student/attendance/{studentId}")
+    public ResponseEntity<BaseResponse<?>> updateAttendance(
+            @PathVariable(value = "studentId") Long studentId,
+            @Valid @RequestBody AttendanceRequest request
+            ) {
+        return ApiResponseUtil.success(SuccessCode.OK, studentService.updateAttendance(studentId, request));
     }
 }
