3a000502bce3220faaa255e68cdb10a757c76492
Jeyong <dlwpdyd201@naver.com>
2025-04-30 11:06:54 +0900

    [FIX] 테스트를 위한 Builder 적용

--- src/main/java/INU/software_design/domain/score/dto/request/StudentScoreRequest.java
+++ src/main/java/INU/software_design/domain/score/dto/request/StudentScoreRequest.java
@@ -1,12 +1,16 @@
 package INU.software_design.domain.score.dto.request;
 
 import INU.software_design.domain.score.entity.SubjectScore;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Getter
+@Builder
+@AllArgsConstructor
 @NoArgsConstructor
 public class StudentScoreRequest {
 
